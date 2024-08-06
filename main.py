97c7b30a5750205e1e09490225c6794a18a9c78f
AlmightyM3 <matthewmmoulton@gmail.com>
2024-08-06 10:12:56 -0400

    Added a transformation matrix

--- main.py
+++ main.py
@@ -12,27 +12,111 @@ from Shader import Shader
 
 dirPath = os.path.dirname(os.path.abspath(__file__))
 
+def translate(matrix, x, y, z):
+    translation_matrix = numpy.array([
+            [1.0, 0.0, 0.0, x  ],
+            [0.0, 1.0, 0.0, y  ],
+            [0.0, 0.0, 1.0, z  ],
+            [0.0, 0.0, 0.0, 1.0],
+        ], dtype=matrix.dtype).T
+    return numpy.dot(matrix, translation_matrix)
+def scale(matrix, x, y, z):
+    translation_matrix = numpy.array([
+            [x  , 0.0, 0.0, 0.0],
+            [0.0, y  , 0.0, 0.0],
+            [0.0, 0.0, z  , 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ], dtype=matrix.dtype).T
+    return numpy.dot(matrix, translation_matrix)
+
+# All further matrix functions are taken from the Pygame-ce glcube example. I hope to rewrite them myself once I actually understand how the math works, but for now this is what I have.
+def rotate(matrix, angle, x, y, z):
+    """
+    Rotate a matrix around an axis.
+
+    :param matrix: The matrix to rotate.
+    :param angle: The angle to rotate by.
+    :param x: x of axis to rotate around.
+    :param y: y of axis to rotate around.
+    :param z: z of axis to rotate around.
+
+    :return: The rotated matrix
+    """
+    angle = math.pi * angle / 180
+    c, s = math.cos(angle), math.sin(angle)
+    n = math.sqrt(x * x + y * y + z * z)
+    x, y, z = x / n, y / n, z / n
+    cx, cy, cz = (1 - c) * x, (1 - c) * y, (1 - c) * z
+    rotation_matrix = numpy.array(
+        [
+            [cx * x + c, cy * x - z * s, cz * x + y * s, 0],
+            [cx * y + z * s, cy * y + c, cz * y - x * s, 0],
+            [cx * z - y * s, cy * z + x * s, cz * z + c, 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=matrix.dtype,
+    ).T
+    matrix[...] = numpy.dot(matrix, rotation_matrix)
+    return matrix
+
+def frustum(left, right, bottom, top, znear, zfar):
+    """
+    Build a perspective matrix from the clipping planes, or camera 'frustrum'
+    volume.
+
+    :param left: left position of the near clipping plane.
+    :param right: right position of the near clipping plane.
+    :param bottom: bottom position of the near clipping plane.
+    :param top: top position of the near clipping plane.
+    :param znear: z depth of the near clipping plane.
+    :param zfar: z depth of the far clipping plane.
+
+    :return: A perspective matrix.
+    """
+    perspective_matrix = numpy.zeros((4, 4), dtype=numpy.float32)
+    perspective_matrix[0, 0] = +2.0 * znear / (right - left)
+    perspective_matrix[2, 0] = (right + left) / (right - left)
+    perspective_matrix[1, 1] = +2.0 * znear / (top - bottom)
+    perspective_matrix[3, 1] = (top + bottom) / (top - bottom)
+    perspective_matrix[2, 2] = -(zfar + znear) / (zfar - znear)
+    perspective_matrix[3, 2] = -2.0 * znear * zfar / (zfar - znear)
+    perspective_matrix[2, 3] = -1.0
+    return perspective_matrix
+
+
+def perspective(fovy, aspect, znear, zfar):
+    """
+    Build a perspective matrix from field of view, aspect ratio and depth
+    planes.
+
+    :param fovy: the field of view angle in the y axis.
+    :param aspect: aspect ratio of our view port.
+    :param znear: z depth of the near clipping plane.
+    :param zfar: z depth of the far clipping plane.
+
+    :return: A perspective matrix.
+    """
+    h = math.tan(fovy / 360.0 * math.pi) * znear
+    w = h * aspect
+    return frustum(-w, w, -h, h, znear, zfar)
+
+
 if __name__ == "__main__":
     # Make a openGL compatable window
     pygame.init()
     WINDOW_SIZE = (800,600)
     window = pygame.display.set_mode(WINDOW_SIZE,  pygame.OPENGL | pygame.DOUBLEBUF)
     clock = pygame.time.Clock()
+    startTime = pygame.time.get_ticks()
     DT = 1.0
     run = True
 
-    vertices = numpy.zeros(4, [("vertex_position", numpy.float32, 3), ("vertex_color", numpy.float32, 3), ("texture_cords", numpy.float32, 2)])
+    vertices = numpy.zeros(4, [("vertex_position", numpy.float32, 3), ("texture_cords", numpy.float32, 2)])
     vertices["vertex_position"] = [
         [ 0.5,  0.5, 0.0],
         [ 0.5, -0.5, 0.0],
         [-0.5, -0.5, 0.0],
         [-0.5,  0.5, 0.0]
-    ]
-    vertices["vertex_color"] = [
-        [0.0, 0.0, 1.0],
-        [0.0, 1.0, 0.0],
-        [1.0, 0.0, 0.0],
-        [0.8, 0.4, 0.6]
     ]
     vertices["texture_cords"] = [
         [1.0, 1.0],
@@ -51,10 +135,8 @@ if __name__ == "__main__":
 
     GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, GL.GL_FALSE, vertices.strides[0], ctypes.c_void_p(0))
     GL.glEnableVertexAttribArray(0)
-    GL.glVertexAttribPointer(1, 3, GL.GL_FLOAT, GL.GL_FALSE, vertices.strides[0], ctypes.c_void_p(vertices.dtype["vertex_position"].itemsize))
+    GL.glVertexAttribPointer(1, 2, GL.GL_FLOAT, GL.GL_FALSE, vertices.strides[0], ctypes.c_void_p(vertices.dtype["vertex_position"].itemsize))
     GL.glEnableVertexAttribArray(1)
-    GL.glVertexAttribPointer(2, 2, GL.GL_FLOAT, GL.GL_FALSE, vertices.strides[0], ctypes.c_void_p(vertices.dtype["vertex_position"].itemsize+vertices.dtype["vertex_color"].itemsize))
-    GL.glEnableVertexAttribArray(2)
 
     indices = numpy.array(
         [
@@ -94,6 +176,10 @@ if __name__ == "__main__":
     theShader.use()
     theShader.setInt("texture1", 0)
     theShader.setInt("texture2", 1)
+
+    trans = numpy.eye(4)
+    transformLoc = GL.glGetUniformLocation(theShader.ID, "transform")
+    GL.glUniformMatrix4fv(transformLoc, 1, GL.GL_FALSE, trans)
     
     while run:
         pygame.display.set_caption(f"3D! | dt:{DT}, fps:{1000/DT}")
@@ -105,7 +191,15 @@ if __name__ == "__main__":
         GL.glBindTexture(GL.GL_TEXTURE_2D, texture1)
         GL.glActiveTexture(GL.GL_TEXTURE1)
         GL.glBindTexture(GL.GL_TEXTURE_2D, texture2)
+
         theShader.use()
+        trans = numpy.eye(4)
+        trans = rotate(trans, (pygame.time.get_ticks()-startTime)*0.05%360,  0.0,0.0,1.0)
+        trans = scale(trans, 0.75,0.75,0.75)
+        trans = translate(trans, 0.5,-0.5,0.0)
+        transformLoc = GL.glGetUniformLocation(theShader.ID, "transform")
+        GL.glUniformMatrix4fv(transformLoc, 1, GL.GL_FALSE, trans)
+
         GL.glBindVertexArray(VAO)
         GL.glDrawElements(GL.GL_TRIANGLES, len(indices), GL.GL_UNSIGNED_INT, None)
 
--- shaders/shader.vert
+++ shaders/shader.vert
@@ -1,14 +1,13 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aTexCord;
+layout (location = 1) in vec2 aTexCord;
+uniform mat4 transform;
 out vec3 Color;
 out vec2 TexCoord;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
-    Color = aColor;
-    TexCoord = aTexCord;//aPos.xy+0.5;
+    gl_Position = transform * vec4(aPos, 1.0f);
+    TexCoord = aTexCord;
 }
