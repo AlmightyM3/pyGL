db8dd3de19a5e3cf10c052615eff2cd3ef963998
AlmightyM3 <matthewmmoulton@gmail.com>
2024-11-16 17:23:43 -0500

    Abstracted lights into Nodes

--- src/Node.py
+++ src/Node.py
@@ -60,7 +60,7 @@ class Transform:
 class Node:
     def __init__(self):
         self.transform = Transform()
-        self.children = []
+        self.children: list[Node] = []
         self.parent: Node = None
 
         self.updateWorldMatrix()
@@ -88,9 +88,9 @@ class Node:
         for child in self.children:
             child.updateWorldMatrix()
     
-    def renderChildren(self, camera, numLights,lightPositions,lightColors,lightFalloffs):
+    def renderChildren(self, camera, lights):
         for child in self.children:
-            child.renderChildren(camera, numLights,lightPositions,lightColors,lightFalloffs)
+            child.renderChildren(camera, lights)
 
 class RenderNode(Node):
     def __init__(self, meshPath="", diffusePath=f"{dirPath}/assets/blank.PNG", specularPath=f"{dirPath}/assets/blank.PNG"):
@@ -106,7 +106,7 @@ class RenderNode(Node):
         self.shader.setInt("material.specular", 1)
         self.shader.setFloat("material.shininess", 32.0)
     
-    def render(self, camera, numLights,lightPositions,lightColors,lightFalloffs):
+    def render(self, camera, lights):
         self.shader.use()
         
         self.shader.setMat4("view", camera.matrix)
@@ -114,19 +114,48 @@ class RenderNode(Node):
         self.shader.setMat4("projection", camera.proj)
         self.shader.setMat4("transform", self.worldMatrix)
 
-        for i in range(numLights):
-            self.shader.setVec3(f"lights[{i}].ambient", lightColors[i]*0.2)
-            self.shader.setVec3(f"lights[{i}].diffuse", lightColors[i]*0.5)
+        for i in range(len(lights)):
+            self.shader.setVec3(f"lights[{i}].ambient", lights[i].color*0.2)
+            self.shader.setVec3(f"lights[{i}].diffuse", lights[i].color*0.5)
             self.shader.setVec3(f"lights[{i}].specular", Vector3(1.0))
-            self.shader.setVec3(f"lights[{i}].position", lightPositions[i])
-            self.shader.setFloat(f"lights[{i}].falloff", lightFalloffs[i])
+            self.shader.setVec3(f"lights[{i}].position", lights[i].transform.position)
+            self.shader.setFloat(f"lights[{i}].falloff", lights[i].falloff)
 
         self.diffuseTexture.use(0)
         self.specularTexture.use(1)
         
         self.mesh.render()
     
-    def renderChildren(self, camera, numLights,lightPositions,lightColors,lightFalloffs):
-        self.render(camera, numLights,lightPositions,lightColors,lightFalloffs)
+    def renderChildren(self, camera, lights):
+        self.render(camera, lights)
         for child in self.children:
-            child.renderChildren(camera, numLights,lightPositions,lightColors,lightFalloffs)
+            child.renderChildren(camera, lights)
+
+class LightNode(Node):
+    def __init__(self, lights, color=Vector3(1), falloff=0.06):
+        super().__init__()
+        self.mesh = Mesh()
+
+        self.transform.scale = Vector3(0.2)
+        self.color = color
+        self.falloff = falloff
+
+        self.shader = Shader(f"{dirPath}/src/shaders/light.vert", f"{dirPath}/src/shaders/light.frag")
+
+        lights.append(self)
+    
+    def render(self, camera):
+        self.shader.use()
+        
+        self.shader.setMat4("view", camera.matrix)
+        self.shader.setVec3("viewPos", camera.position)
+        self.shader.setMat4("projection", camera.proj)
+        self.shader.setMat4("transform", self.worldMatrix)
+        self.shader.setVec3("lightColor", self.color)
+        
+        self.mesh.render()
+    
+    def renderChildren(self, camera, lights):
+        self.render(camera)
+        for child in self.children:
+            child.renderChildren(camera, lights)
--- src/main.py
+++ src/main.py
@@ -2,19 +2,15 @@ import os
 import pygame
 import OpenGL.GL as GL
 import OpenGL.GLU as GLU
-import numpy
 
 from pygame import Vector3
 
-from Shader import Shader
 from Camera import *
-from Mesh import Mesh
 from Node import *
 
 dirPath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if "\\" in dirPath:
     dirPath = dirPath.replace("\\", "/")
-NUM_POINT_LIGHTS = 2
 WINDOW_SIZE = (800,600)
 
 
@@ -30,33 +26,30 @@ if __name__ == "__main__":
     pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 4)
     print(f"OpenGL version {pygame.display.gl_get_attribute(pygame.GL_CONTEXT_MAJOR_VERSION)}.{pygame.display.gl_get_attribute(pygame.GL_CONTEXT_MINOR_VERSION)}")
 
-    lightMesh = Mesh()
+    lights: list[LightNode] = []
 
     rootNode = Node()
 
-    cubeNode = RenderNode(meshPath=f"{dirPath}/assets/Suzanne.obj")
+    cubeNode1 = RenderNode(meshPath=f"{dirPath}/assets/Suzanne.obj")
     cubeNode2 = RenderNode(diffusePath=f"{dirPath}/assets/container.PNG", specularPath=f"{dirPath}/assets/container_specular.PNG")
     cubeNode3 = RenderNode(diffusePath=f"{dirPath}/assets/container.PNG", specularPath=f"{dirPath}/assets/container_specular.PNG")
-    cubeNode.transform.scale = Vector3(0.7)
-    cubeNode.setParent(rootNode)
+    cubeNode1.transform.scale = Vector3(0.7)
+    cubeNode1.setParent(rootNode)
     cubeNode2.transform.position = Vector3(4,0,0)
     cubeNode2.transform.scale = Vector3(1.2)
-    cubeNode2.setParent(cubeNode)
+    cubeNode2.setParent(cubeNode1)
     cubeNode3.transform.position = Vector3(1.3,0,0)
     cubeNode3.transform.scale = Vector3(0.5)
     cubeNode3.setParent(cubeNode2)
-    cubeNode.updateWorldMatrix()
+
+    light1=LightNode(lights)
+    light2=LightNode(lights)
+    light1.transform.position = Vector3(1.2,1.0,2.0)
+    light2.transform.position = Vector3(-3,5,-8)
+    light1.setParent(rootNode)
+    light2.setParent(rootNode)
 
     camera = FreeCamera(WINDOW_SIZE, startPos=Vector3(0.0, 0.0, 3.0))
-
-    lightPositions = [Vector3(1.2,1.0,2.0), Vector3(-3,5,-8)]
-    lightColors = [Vector3(1.0,1.0,1.0), Vector3(1.0,1.0,1.0)]
-    lightFalloffs = [0.06, 0.06]
-
-    lightShader = Shader(f"{dirPath}/src/shaders/light.vert", f"{dirPath}/src/shaders/light.frag")
-    lightShader.use()
-    lightShader.setMat4("view", camera.matrix)
-    lightShader.setMat4("projection", camera.proj)
 
     GL.glEnable(GL.GL_DEPTH_TEST)
     
@@ -64,24 +57,17 @@ if __name__ == "__main__":
         pygame.display.set_caption(f"3D! | dt:{dt}, fps:{1000/dt}")
 
         camera.Update(dt)
+
+        cubeNode1.transform.rotationAngle += 0.05 * dt
+        cubeNode2.transform.rotationAngle += 0.1 * dt
+        cubeNode3.transform.rotationAngle += 0.15 * dt
+
+        rootNode.updateWorldMatrix()
         
         GL.glClearColor(0.1, 0.1, 0.1, 1.0)#0.2, 0.3, 0.3, 1.0
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
-
-        cubeNode.transform.rotationAngle += 0.05 * dt
-        cubeNode2.transform.rotationAngle += 0.1 * dt
-        cubeNode3.transform.rotationAngle += 0.15 * dt
         
-        rootNode.updateWorldMatrix()
-        
-        lightShader.use()
-        lightShader.setMat4("view", camera.matrix)
-        for i in range(NUM_POINT_LIGHTS):
-            lightShader.setMat4("transform", translate(scale(numpy.eye(4), 0.2,0.2,0.2), lightPositions[i].x,lightPositions[i].y,lightPositions[i].z))
-            lightShader.setVec3("lightColor", lightColors[i])
-            lightMesh.render()
-        
-        rootNode.renderChildren(camera, NUM_POINT_LIGHTS,lightPositions,lightColors,lightFalloffs)
+        rootNode.renderChildren(camera, lights)
 
         pygame.display.flip()
         dt = clock.tick(500)
